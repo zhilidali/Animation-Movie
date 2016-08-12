45cd2aa7abb7b44a829066d8a00d38915135d2ca
zhilidali <1373653244@qq.com>
2016-08-13 05:34:37 +0800

    Node site

---
+++ README.md
@@ -0,0 +1,11 @@
+# 1.执行程序文件，在终端输入`node meadowlark`
+
+## 本例采用Handlebars作为视图引擎
+
+# 目录
+## 1.package.json 不解释
+## 2.index.js 主程序文件
+## 3.views 视图文件夹
+		layouts 放置布局/母版页的文件夹
+## 4.public 放置静态资源
+## 5.lib 保存模块的目录
---
+++ index.js
@@ -0,0 +1,45 @@
+//主程序入口文件
+var express = require('express');
+var mymodule = require('./lib/mymodule');
+var app = express();
+
+
+var handlebars = require('express3-handlebars')
+					.create({
+						defaultLayout: 'main'
+					});
+app.engine('handlebars', handlebars.engine);//设置模板引擎，处理指定的后缀名文件
+app.set('view engine', 'handlebars');//指定渲染模板文件的后缀名
+
+app.set('port', process.env.PORT || 3000);
+
+//static 中间件,express.static指定静态文件的查找目录
+app.use(express.static(__dirname+'/public'));
+
+//路由
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/about', function(req, res) {
+	res.render('about', {
+		MovieName: mymodule.getMovieName()
+	});
+});
+
+//404页面
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(404);
+	res.render('404');
+});
+
+//500页面
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Start on http://localhost:'+app.get('port')+'; 摁Ctrl+C结束终端terminate')
+})
